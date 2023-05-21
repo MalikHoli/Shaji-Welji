4d7f8f5101423174031ebe6fdf8bcf53e9bdcc57
Mallikarjun <mallikarjunabhi@gmail.com>
2023-05-21 13:18:09 +0530

    Inventory Page for Shahji

--- App.js
+++ App.js
@@ -36,6 +36,72 @@ app.post('/LoginData', async (request, response) => {
 })
 //-----------------------------------------------------------------------------------------
 
+//--------------------Serving the Product list for home page dropdown---------------------
+app.post('/ProductData', async (request, response) => {
+    try {
+        const vProductList = await ReadFromDB("Products");
+        let vProductArray = [];
+        for (let i = 1; i <= vProductList.length - 1; i++) {
+            vProductArray.push(vProductList[i][0])
+        }
+        let vUnitArray = [];
+        for (let i = 1; i <= vProductList.length - 1; i++) {
+            vUnitArray.push(vProductList[i][1])
+        }
+        response.json({
+            status: "successful",
+            "Products": vProductArray,
+            "Units": vUnitArray
+        })
+    } catch (error) {
+        response.send(error);
+    }
+})
+//-----------------------------------------------------------------------------------------
+
+//--------------------Serving the Unit value for selected product-----------------------
+app.post('/UnitData', async (request, response) => {
+    try {
+        const vProductList = await ReadFromDB("Products");
+        for (let i = 1; i <= vProductList.length - 1; i++) {
+            if (vProductList[i][0] == request.body.SelectedProduct) {
+                response.json({
+                    status: "successful",
+                    "Unit": vProductList[i][1]
+                })
+                break
+            }
+        }
+    } catch (error) {
+        response.send(error);
+    }
+})
+//-----------------------------------------------------------------------------------------
+
+
+//--------------------Serving the Unit value for selected product-----------------------
+app.post('/InventoryData', async (request, response) => {
+    try {
+        let vData = request.body;
+        let vDataArray = [[vData["Timestamp"]
+            ,vData["Product Name"], vData["Purchase Cost"]
+            ,vData["Purchase Quantity"], vData["Unit"]
+            ,vData["Warehouse Location"], vData["Note"]
+        ]];
+        const vUpdatedRow = await writeToGoogleSheet(vDataArray, 1,'Inventory');
+        const vUpdatedData = await ReadFromDB(vUpdatedRow);
+        const vHeaderData = await ReadFromDB("Inventory!A1:G1");
+            response.json({
+                status: "successful",
+                "Row updated": vUpdatedRow,
+                "Data": [vHeaderData[0],vUpdatedData[0]]
+            })
+    } catch (error) {
+        response.send(error);
+    }
+})
+//-----------------------------------------------------------------------------------------
+
 /*1*/
 async function ReadFromGoogleSheetDB() {
     const privateKey = fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS);
@@ -51,7 +117,109 @@ async function ReadFromGoogleSheetDB() {
         spreadsheetId: sheetId, range: range
     }
     try {
-        const response = [(await sheets.spreadsheets.values.get(request)).data.values[2][2],(await sheets.spreadsheets.values.get(request)).data.values[2][1]];
+        const response = [(await sheets.spreadsheets.values.get(request)).data.values[2][2], (await sheets.spreadsheets.values.get(request)).data.values[2][1]];
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
+
+/*2*/
+async function ReadFromDB(Range) {
+    const privateKey = fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+    const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
+    const jwtClient = new GoogleAuth({
+        key: privateKey,
+        scopes: scopes
+    });
+    const sheets = google.sheets({ version: "v4", auth: jwtClient });
+    const sheetId = "1kFjvRciwtIZS9JBmT19h8Zrp47A_sDYlmO_iL7AB69Y";
+    const range = Range;
+    const request = {
+        spreadsheetId: sheetId, range: range
+    }
+    try {
+        const response = (await sheets.spreadsheets.values.get(request)).data.values
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
+
+/*3*/
+async function writeToGoogleSheet(values, Row, Sheet) {
+    const privateKey = fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+    const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
+    const jwtClient = new GoogleAuth({
+        key: privateKey,
+        scopes: scopes
+    });
+    const sheets = google.sheets({ version: "v4", auth: jwtClient });
+
+    let request;
+    if (Row == 1) { // To append row
+        request = {
+            spreadsheetId: "1kFjvRciwtIZS9JBmT19h8Zrp47A_sDYlmO_iL7AB69Y",
+            //ReadFromGoogleSheet() will return the bank cell rang
+            range: `${Sheet}!A${await ReadFromGoogleSheet(Sheet)}`,
+            valueInputOption: "USER_ENTERED",
+            insertDataOption: "INSERT_ROWS",
+            resource: {
+                values: values
+            }
+        };
+    } else { // To update row
+        request = {
+            spreadsheetId: "1kFjvRciwtIZS9JBmT19h8Zrp47A_sDYlmO_iL7AB69Y",
+            range: `${Sheet}!A${Row}`,
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: values
+            }
+        };
+    }
+    if (Row == 1) { // To append row
+        try {
+            const response = (await sheets.spreadsheets.values.append(request)).data.updates.updatedRange;
+            const CellsUpdated = `${response}`;
+            return CellsUpdated;
+        } catch (error) {
+            return error;
+        }
+    } else { // To update row
+        try {
+            const response = (await sheets.spreadsheets.values.update(request)).data.updatedRange;
+            const CellsUpdated = `${response}`;
+            return CellsUpdated;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    //*************************Below is one more way to wrire above code*****************************//
+    // sheets.spreadsheets.values.append(request, (err, res) => {
+    //     if (err) return console.log(`The API returned an error: ${err}`);
+    //     console.log(res.data);
+    // });
+    //**********************************************************************************************//
+}
+
+/*2*/
+async function ReadFromGoogleSheet(Range) {
+    const privateKey = fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+    const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
+    const jwtClient = new GoogleAuth({
+        key: privateKey,
+        scopes: scopes
+    });
+    const sheets = google.sheets({ version: "v4", auth: jwtClient });
+    const sheetId = "1kFjvRciwtIZS9JBmT19h8Zrp47A_sDYlmO_iL7AB69Y";
+    const range = Range;
+    const request = {
+        spreadsheetId: sheetId, range: range
+    }
+    try {
+        const response = (await sheets.spreadsheets.values.get(request)).data.values.length + 1;
         return response;
     } catch (error) {
         return error;
